5d0d9406df61ed55b4601e27244300b9e398bbee
jamoozy <jamoozy@9499b3bb-8867-ee54-9472-c902d83d7ad1>
2010-08-28 15:32:02 +0000

    Started the move to gtk.Builder.

--- exporter.py
+++ exporter.py
@@ -1,8 +1,6 @@
 import math
 import cairo
 
-import deskcorder as dc
-
 def _draw_slide_on_surface(ctx, slide, scale = (400,300)):
   for stroke in slide.strokes:
     ctx.set_source_rgb(stroke.r(), stroke.g(), stroke.b())
@@ -15,26 +13,28 @@ def _draw_slide_on_surface(ctx, slide, scale = (400,300)):
       ctx.line_to(point.x() * scale[0], point.y() * scale[1])
     ctx.stroke()
 
-def to_pdf(trace, ofname, size = (400,300)):
+def to_pdf(trace, ofname, size = (400,300), times = None):
   surface = cairo.PDFSurface("%s.pdf" % ofname, size[0], size[1])
   ctx = cairo.Context(surface)
-  for slide in trace.slides:
-    _draw_slide_on_surface(ctx, slide, size)
-    ctx.show_page()
+  if times is None:
+    for slide in trace.slides:
+      _draw_slide_on_surface(ctx, slide, size)
+      ctx.show_page()
 
-def to_png(trace, ofname, size = (400,300)):
+def to_png(trace, ofname, size = (400,300), times = None):
   slideidx = 0
-  for slide in trace.slides:
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, size[0], size[1])
-    ctx = cairo.Context(surface)
-    ctx.set_source_rgb(1.0,1.0,1.0)
-    ctx.paint()
-    _draw_slide_on_surface(ctx, slide, size)
-    surface.write_to_png("%s-%03d.png" % (ofname,slideidx))
-    slideidx += 1
+  if times is None:
+    for slide in trace.slides:
+      surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, size[0], size[1])
+      ctx = cairo.Context(surface)
+      ctx.set_source_rgb(1.0,1.0,1.0)
+      ctx.paint()
+      _draw_slide_on_surface(ctx, slide, size)
+      surface.write_to_png("%s-%03d.png" % (ofname,slideidx))
+      slideidx += 1
 
 if __name__ == "__main__":
-    import sys, recorder
-    trace,audio = recorder.load(sys.argv[1])
-    to_pdf(trace, sys.argv[2])
-    to_png(trace, sys.argv[2])
+  import sys, recorder
+  trace,audio = recorder.load(sys.argv[1])
+  to_pdf(trace, sys.argv[2])
+  to_png(trace, sys.argv[2])
