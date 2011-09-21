d7673dc76768556cfdc93f17306d001a208ca4dd
jamoozy <jamoozy@9499b3bb-8867-ee54-9472-c902d83d7ad1>
2011-09-21 06:17:10 +0000

    Fixed path of "layout.gtk".  This should have been done with the  initial moving update.

--- trunk/src/linux.py
+++ trunk/src/linux.py
@@ -272,7 +272,7 @@ class GUI:
 
     # Set up most of the window (from XML file).
     self.builder = gtk.Builder()
-    self.builder.add_from_file('layout.gtk')
+    self.builder.add_from_file('../layout.gtk')
     self['root'].connect("destroy", lambda x: sys.exit(0))
 
     # Add the canvas, too.
