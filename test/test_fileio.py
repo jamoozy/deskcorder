185e4eb5b54cc4c45234f00c76160e7a5a7fd413
jamoozy <jamoozy@9499b3bb-8867-ee54-9472-c902d83d7ad1>
2011-09-29 20:14:22 +0000

    Put basic test cases into test_fileio.py.

--- test/test_fileio.py
+++ test/test_fileio.py
@@ -12,7 +12,7 @@ import datatypes
 
 class TestDCT(unittest.TestCase):
   def setUp(self):
-    self.dct = fileio.DCT(tempfile.NamedTemporaryFile().name)
+    self.dct = fileio.DCT(tempfile.NamedTemporaryFile(prefix='TestDCT-').name)
 
   def test_init(self):
     self.assertNotEqual(None, self.dct.fname)
@@ -24,21 +24,22 @@ class TestDCT(unittest.TestCase):
   def test_simple_lecture(self):
     lec = datatypes.Lecture()
     lec.append(datatypes.Start(12345, (500,400)))
+    lec.append(datatypes.Color(12345, (1, 0, 0)))
     lec.append(datatypes.Click(12678, (40, 80)))
     lec.append(datatypes.Point(16789, (40, 90), 1))
     lec.append(datatypes.Point(23456, (50, 90), 1))
     lec.append(datatypes.Point(24567, (60, 90), 1))
     lec.append(datatypes.Point(25678, (80, 90), 1))
     lec.append(datatypes.Release(34567, (90,100)))
-    
+    lec.append(datatypes.End(40000, (500,400)))
+
     self.dct.save(lec)
     l2 = self.dct.load()
-    
     self.assertFalse(l2.is_empty())
 
 class TestDCB(unittest.TestCase):
   def setUp(self):
-    self.dcb = fileio.DCB(tempfile.NamedTemporaryFile().name)
+    self.dcb = fileio.DCB(tempfile.NamedTemporaryFile(prefix='TestDCB-').name)
 
   def test_init(self):
     self.assertNotEqual(None, self.dcb.fname)
@@ -47,9 +48,26 @@ class TestDCB(unittest.TestCase):
     self.assertEqual(None, self.dcb.log)
     self.assertEqual(None, self.dcb.lec)
 
+  def test_simple_lecture(self):
+    lec = datatypes.Lecture()
+    lec.append(datatypes.Start(12345, (500,400)))
+    lec.append(datatypes.Color(12345, (1, 0, 0)))
+    lec.append(datatypes.Click(12678, (40, 80)))
+    lec.append(datatypes.Point(16789, (40, 90), 1))
+    lec.append(datatypes.Point(23456, (50, 90), 1))
+    lec.append(datatypes.Point(24567, (60, 90), 1))
+    lec.append(datatypes.Point(25678, (80, 90), 1))
+    lec.append(datatypes.Release(34567, (90,100)))
+    lec.append(datatypes.End(40000, (500,400)))
+
+    self.dcb.save(lec)
+    l2 = self.dcb.load()
+
+    self.assertFalse(l2.is_empty())
+
 class TestDCD(unittest.TestCase):
   def setUp(self):
-    self.dcd = fileio.DCD(tempfile.NamedTemporaryFile().name)
+    self.dcd = fileio.DCD(tempfile.NamedTemporaryFile(prefix='TestDCD-').name)
     
   def test_init(self):
     self.assertNotEqual(None, self.dcd.fname)
@@ -57,6 +75,23 @@ class TestDCD(unittest.TestCase):
     self.assertEqual(None, self.dcd.fp)
     self.assertEqual(None, self.dcd.log)
     self.assertEqual(None, self.dcd.lec)
+
+  def test_simple_lecture(self):
+    lec = datatypes.Lecture()
+    lec.append(datatypes.Start(12345, (500,400)))
+    lec.append(datatypes.Color(12345, (1, 0, 0)))
+    lec.append(datatypes.Click(12678, (40, 80)))
+    lec.append(datatypes.Point(16789, (40, 90), 1))
+    lec.append(datatypes.Point(23456, (50, 90), 1))
+    lec.append(datatypes.Point(24567, (60, 90), 1))
+    lec.append(datatypes.Point(25678, (80, 90), 1))
+    lec.append(datatypes.Release(34567, (90,100)))
+    lec.append(datatypes.End(40000, (500,400)))
+
+    self.dcd.save(lec)
+    l2 = self.dcd.load()
+
+    self.assertFalse(l2.is_empty())
 
 
 if __name__ == "__main__":
